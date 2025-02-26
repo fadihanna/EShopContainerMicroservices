01d9ce8d88f6e9a5e54f68c141022ea30369a9b9
George Akladyos <georgemaged900@aucegypt.edu>
2025-02-26 13:19:37 +0200

    add grpc,payment gateway DI, fix inputparamlist dto namespace

--- src/Services/MagicServices/Magic.Application/Dtos/Common/InquiryRequestDto.cs
+++ src/Services/MagicServices/Magic.Application/Dtos/Common/InquiryRequestDto.cs
@@ -1,11 +1,7 @@
-﻿namespace Magic.Application.Dtos.Common;
+﻿namespace BuildingBlocks.Models;
 //this is the client request coming from "frontend"
 public record InquiryRequestDto(
     List<InputParameter> InputParameterList,
     int DenominationId,
     string BillingAccount
-);
-public record InputParameter(
-    string Key,
-    string Value
 );
